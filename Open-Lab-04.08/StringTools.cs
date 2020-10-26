f7826b19e0b4872f91fbb5493376a52335d1752f
tmichalikova231@gmail.com <tmichalikova231@gmail.com>
2020-10-26 21:24:31 +0100

    Done

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Open_Lab_04._08
 {
     public class StringTools
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> str = new List<string>();
+            foreach (string four in strings)
+            {
+                if (four.Length == 4)
+                {
+                    str.Add(four);
+                }
+            }
+            return str.ToArray();
         }
     }
 }
